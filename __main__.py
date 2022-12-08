d23e04017ebf683a008734ffa5e262d1c3e49303
doggystylez <98429202+doggystylez@users.noreply.github.com>
2022-12-08 14:48:06 -0500

    add account number for offline signing

--- __main__.py
+++ __main__.py
@@ -8,7 +8,6 @@ import time
 import toml
 from __init__ import bcolors, __location__
 
-
 def print_sig(signed, file):
     print(f"\nprinting signatures now. you can copy and paste this output")
     time.sleep(1)
@@ -17,17 +16,13 @@ def print_sig(signed, file):
             print(
                 f"{bcolors.magenta}\n----------------------sig {bcolors.blue}{sig}{bcolors.magenta} begins----------------------\n{bcolors.grey}"
                 + json.dumps(json.load(f), indent=1)
-                + f"{bcolors.magenta}\n----------------------sig {bcolors.blue}{sig}{bcolors.magenta} ends----------------------{bcolors.nc}"
-            )
-
+                + f"{bcolors.magenta}\n----------------------sig {bcolors.blue}{sig}{bcolors.magenta} ends----------------------{bcolors.nc}")
 
 def main():
-
     txs = sys.argv[1:]
     if txs == []:
         print(
-            f"{bcolors.red}error: no txs provided {bcolors.nc}(select transactions like 'python3 multisigner 0 1' for transactions 0 and 1)"
-        )
+            f"{bcolors.red}error: no txs provided {bcolors.nc}(select transactions like 'python3 multisigner 0 1' for transactions 0 and 1)")
         exit()
 
     try:
@@ -38,8 +33,7 @@ def main():
         print(
             f"{bcolors.red}error{bcolors.nc}: config file not found.\n\n"
             f"quick setup:\n\n  1. {bcolors.blue}cp ./multisigner/config.toml ./multisigner/.config.toml{bcolors.nc}\n"
-            f"  2. {bcolors.blue}nano multisigner/.config.toml{bcolors.nc} and fill in your info\n  3. run this program again\n"
-        )
+            f"  2. {bcolors.blue}nano multisigner/.config.toml{bcolors.nc} and fill in your info\n  3. run this program again\n")
         exit()
 
     try:
@@ -56,16 +50,18 @@ def main():
                 "--work-tree", f"./{settings['REPO'].split('/')[1]}", "pull", "--quiet"])
     except (subprocess.CalledProcessError, FileNotFoundError):
         NAME = ""
-    try:
-        acct = json.loads(
-            subprocess.check_output(
+    if settings["ACCOUNT"]:
+        acct = settings["ACCOUNT"]
+    else:
+        try:
+            acct = json.loads(
+                subprocess.check_output(
                     ["osmosisd", "q", "account", settings["KEY"], "--node", settings["RPC"], "--output", "json"]).decode("utf-8")
-        )["account_number"]
-    except subprocess.CalledProcessError:
-        print(
-            f"{bcolors.red}rpc error. please check your connection, try a different node, or wait and try again"
-        )
-        exit()
+                )["account_number"]
+        except subprocess.CalledProcessError:
+            print(
+                f"{bcolors.red}rpc error. please check your connection, try a different node, or wait and try again")
+            exit()
     if NAME:
         FILE = f"-{NAME}-sig.json"
     else:
@@ -75,8 +71,7 @@ def main():
     signed = []
     for tx in txs:
         r = requests.get(
-            f"https://raw.githubusercontent.com/{settings['REPO']}/main/transactions/unsigned/{tx}.json"
-        )
+            f"https://raw.githubusercontent.com/{settings['REPO']}/main/transactions/unsigned/{tx}.json")
         with open(f"/tmp/{tx}.json", "w") as f:
             f.write(r.text)
         print(f"\nplease check tx {bcolors.blue}{tx}{bcolors.nc} before signing:\n")
@@ -84,8 +79,7 @@ def main():
             print(
                 bcolors.yellow
                 + json.dumps(json.load(f)["body"]["messages"], indent=1)
-                + f"\n{bcolors.nc}"
-            )
+                + f"\n{bcolors.nc}")
         try:
             subprocess.check_output(
                     ["osmosisd", "tx", "sign", f"/tmp/{tx}.json", "--from", settings["KEY"],
@@ -95,19 +89,16 @@ def main():
                 signed.append(tx)
         except subprocess.CalledProcessError:
             print(
-                f"sig {tx} {bcolors.red}failed{bcolors.nc}. please check the tx or your config"
-            )
+                f"sig {tx} {bcolors.red}failed{bcolors.nc}. please check the tx or your config")
 
     if signed != []:
         if NAME:
             choice = input(
-                "signatures completed\n\n   1) upload signatures to repo\n   2) print signatures to console\n\nyour choice: "
-            )
+                "signatures completed\n\n   1) upload signatures to repo\n   2) print signatures to console\n\nyour choice: ")
             while choice != "1" and choice != "2":
                 time.sleep(0.1)
                 choice = input(
-                    "invalid input. please enter 1 to upload or 2 to print signatures: "
-                )
+                    "invalid input. please enter 1 to upload or 2 to print signatures: ")
         else:
             choice = "2"
     else:
@@ -135,15 +126,13 @@ def main():
             print(f"\n{bcolors.green}signatures uploaded to repo{bcolors.nc}")
         except subprocess.CalledProcessError:
             print(
-                f"{bcolors.red}error uploading signatures to repo{bcolors.nc}\n\nprinting now..."
-            )
+                f"{bcolors.red}error uploading signatures to repo{bcolors.nc}\n\nprinting now...")
             print_sig(signed, FILE)
     elif choice == "2":
         print_sig(signed, FILE)
     else:
         print("fatal error")
         exit()
-
 
 if __name__ == "__main__":
     main()
