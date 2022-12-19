562c411f5a974ba49106642c7510220126fc3d9f
doggystylez <98429202+doggystylez@users.noreply.github.com>
2022-12-19 17:17:54 -0500

    error handling

--- scripts/osmosis_tx_maker.sh
+++ scripts/osmosis_tx_maker.sh
@@ -3,22 +3,11 @@
 printf "enter "from" address: "
 read -r key
 
-function create_message {
-case "$1" in "send"*)
-tx=$(osmosisd tx bank $1 --from $key --generate-only)
-;; "grant"*)
-tx=$(osmosisd tx authz $1 --from $key --generate-only)
-;; "vote"*)
-tx=$(osmosisd tx gov $1 --from $key --generate-only)
-;; *)
-printf "error - tx not generated - check your inputs\n"
-;; esac
-if [ ! -z "$2" ] ; then
-message=$(jq '.body.messages' <<<$tx)
-tx=$(jq ".body.messages += $message" <<<$2)
-fi
-[ -z "$tx" ] && { printf "exiting\n" ; exit ; }
-add_message "$tx"
+function build_tx {
+  [ -z "$1" ] && tx_error "$2"
+  message=$(jq '.body.messages' <<<$1 2> /dev/null)
+  [ -z "$2" ] && tx="$1" || tx=$(jq ".body.messages += $message" <<<$2 2> /dev/null)
+  add_message "$tx"
 }
 
 function add_message {
@@ -33,18 +22,41 @@ function add_message {
     read -r n_message
     create_message "$n_message" "$1"
   else
-    printf "\nyour completed tx:\n\n"
-    jq . <<<$1
+    print_tx "$1"
   fi
 }
 
-printf "enter the first tx message in an abreviated manner like such:\n\nsend tx:\nsend <from> <to> <amount>\nfor a vote tx:\nvote 69 yes\n\nsupported messages:\nsend, vote, \n\nyour message: "
+function create_message {
+    case "$1" in "send"*)
+  x=${1#"send"}
+  tx=$(osmosisd tx bank send $key $x  --from $key --generate-only)
+    ;; "grant"*)
+  tx=$(osmosisd tx authz $1 --from $key --generate-only)
+    ;; "vote"*)
+  tx=$(osmosisd tx gov $1 --from $key --generate-only)
+    ;; esac
+  build_tx "$tx" "$2"
+}
+
+function tx_error {
+  printf "error - last input invalid - try again: "
+  read -r again
+  create_message "$again" "$1"
+}
+
+function print_tx {
+  printf "\nyour completed tx:\n\n"
+  jq . <<<$1
+  # printf "\nenter a file name to write to file: "
+  # read -r file
+  # cat $tx > $file
+  exit
+}
+
+printf "enter the first tx message in an abreviated manner like such:\n\nsend tx:\nsend <to> <amount>\nfor a vote tx:\nvote 69 yes\n\nsupported messages:\nsend, vote, \n\nyour message: "
 read -r u_message
 if [ ! -z "$u_message" ] ; then
   create_message "$u_message"
-# printf "\nenter a sequence number or tx name to save to file: "
-# read -r file
-# cat $tx > $file
 else
   printf "error - no input\n"
 fi
