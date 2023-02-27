5cbf547c829322ec463806e57d81610ac5b100c9
doggystylez <98429202+doggystylez@users.noreply.github.com>
2023-02-26 23:36:50 -0500

    improve args

--- __main__.py
+++ __main__.py
@@ -3,10 +3,10 @@ from os import path
 from sys import argv
 from time import sleep
 from json import dumps,load,loads
-from config import client,multisig
+from importlib import import_module
+from getopt import getopt,GetoptError
 from __init__ import bcolors, __location__
 from subprocess import check_output,CalledProcessError
-
 
 def print_sig(signed):  # print signatures to console
     print(f"\nprinting signatures now. you can copy and paste this output")
@@ -19,34 +19,52 @@ def print_sig(signed):  # print signatures to console
                 + f"{bcolors.magenta}\n----------------------sig {bcolors.blue}{sig['tx']}{bcolors.magenta} ends----------------------{bcolors.nc}")
 
 def main():
+    config_file = "config"  # default config file
+    arg_list = argv[1:]  # check command line args
     try:
-        check_output(["which", "osmosisd"])
-    except CalledProcessError:
-        print(f"{bcolors.red}error: osmosisd not installed.{bcolors.nc} please see <link> for a guide")
+        args,txs = getopt(arg_list, "hac:")
+        for arg,val in args:
+            if arg in "-h":
+                print("help menu")  # to do
+                exit()
+            if arg in "-c":  # custom config
+                if val.endswith(".py"):
+                    config_file = val[:-3]
+                else:
+                    config_file = val
+#            if arg in "-a":
+#                txs = "all"
+    except GetoptError as err:
+        print(f"{bcolors.yellow}error - {str(err)}{bcolors.nc}")
         exit()
-    if not multisig["signer_address"] or not multisig["multisig_address"]:
+    try:
+        config = import_module(config_file)
+    except ModuleNotFoundError:
+        print(
+            f"{bcolors.red}error{bcolors.nc}: config file not found.\n\n"
+            f"quick setup:\n\n  1. {bcolors.blue}`cp ./multisigner/config.py.sample ./multisigner/config.py`{bcolors.nc}\n"
+            f"  2. {bcolors.blue}`nano multisigner/config.py`{bcolors.nc} and fill in your info\n")
+        exit()
+    if not config.client["binary"]:
+        print(f"{bcolors.red}error: no binary name in config{bcolors.nc}")
+        exit()
+    else:
+        binary = config.client["binary"]
+    try:
+        check_output(["which", binary])
+    except CalledProcessError:
+        print(f"{bcolors.red}error: {binary} not installed or not in path.{bcolors.nc} please see <link> for a guide")
+        exit()
+    if not config.multisig["signer_address"] or not config.multisig["multisig_address"]:
         print(f"{bcolors.red}error: signer and/or multisig address not configured{bcolors.nc}")
         exit()
-    txs = argv[1:]  # check command line args
     if txs == []:
         print(f"{bcolors.red}error: no txs provided {bcolors.nc}(select transactions like 'python3 multisigner 0 1' for transactions 0 and 1)")
         exit()
     try:
-        if not client["binary"]:
-            print(f"{bcolors.red}error: no binary name in config{bcolors.nc}")
-            exit()
-        else:
-            binary = client["binary"]
-    except FileNotFoundError:
-        print(
-            f"{bcolors.red}error{bcolors.nc}: config file not found.\n\n"
-            f"quick setup:\n\n  1. {bcolors.blue}cp ./multisigner/config.py.sample ./multisigner/config.py{bcolors.nc}\n"
-            f"  2. {bcolors.blue}nano multisigner/config.py{bcolors.nc} and fill in your info\n  3. run this program again\n")
-        exit()
-    try:
         check_output(["git", "config", "user.email"])  # check if git email configured
-        if multisig["repo"]:
-            repo = multisig["repo"]
+        if config.multisig["repo"]:
+            repo = config.multisig["repo"]
             repo_dir = path.join(__location__, repo.split('/')[1])
             if not path.exists(repo_dir):  # if the repo exists in config, make sure we have it and pull changes
                 check_output(
@@ -59,16 +77,16 @@ def main():
     except (CalledProcessError,FileNotFoundError):
         print(f"{bcolors.yellow}warn: git not configured{bcolors.nc}")
         repo = "null"
-    if multisig["signer_name"]:
-        name = multisig["signer_name"]
+    if config.multisig["signer_name"]:
+        name = config.multisig["signer_name"]
     else:
         name = "null"
-    if not client["rpc"]:
+    if not config.client["rpc"]:
         RPC = "null"
     else:
-        RPC = client["rpc"]
-    if multisig["account_number"]:
-        acct = multisig["account_number"]  # get the account number from config or rpc call
+        RPC = config.client["rpc"]
+    if config.multisig["account_number"]:
+        acct = config.multisig["account_number"]  # get the account number from config or rpc call
     else:
         if RPC == "null":
             print("no rpc or account number in config. you need one or the other")
@@ -76,7 +94,7 @@ def main():
         else:
             try:
                 acct = loads(
-                    check_output([binary, "q", "account", multisig["multisig_address"], "--node", RPC, "--output", "json"]).decode("utf-8"))["account_number"]
+                    check_output([binary, "q", "account", config.multisig["multisig_address"], "--node", RPC, "--output", "json"]).decode("utf-8"))["account_number"]
             except CalledProcessError:
                 print(f"{bcolors.red}rpc error.{bcolors.nc} please check your connection, try a different node, or wait and try again - or add your account number to config for offline mode")
                 exit()
@@ -100,7 +118,8 @@ def main():
             exit()
         print(f"\nplease check tx {bcolors.blue}{tx}{bcolors.nc} before signing:\n")  # print the "messages" to console for review
         with open(tx_file) as f:
-            print(f"{bcolors.yellow}{dumps(load(f)['body']['messages'], indent=1)}\n{bcolors.nc}")
+            body = load(f)['body']
+        print(f"{bcolors.yellow}{dumps(body['messages'], indent=1)}{bcolors.nc}\n")
         if tx.split('-')[0].isnumeric():  # if the tx begins with a number and a dash, we assume its the sequence number, if not we ask for one
             seq = tx.split('-')[0]
         else:
@@ -110,16 +129,16 @@ def main():
                     print("error - no rpc in config")
                 else:
                     try:
-                        seq = loads(check_output([binary, "q", "account", multisig["signer_address"], "--node", RPC, "--output", "json"]).decode("utf-8"))["sequence"]
+                        seq = loads(check_output([binary, "q", "account", config.multisig["signer_address"], "--node", RPC, "--output", "json"]).decode("utf-8"))["sequence"]
                     except CalledProcessError:
                         print(f"{bcolors.red}rpc error. please check your connection, try a different node, or wait and try again - or enter sequence number")
             while not seq.isnumeric():
                 seq = input("error - sequence number invalid. input sequence number if known: ")
         try:
             check_output(  #  sign the txs
-                    [binary, "tx", "sign", tx_file, "--from", multisig["signer_address"],
-                    "--multisig", multisig["multisig_address"], "--keyring-backend", client["keyring"], "--account-number", acct, "--sequence", seq,
-                    "--chain-id", client["chain_id"], "--offline", "--output-document", sig_file])
+                    [binary, "tx", "sign", tx_file, "--from", config.multisig["signer_address"],
+                    "--multisig", config.multisig["multisig_address"], "--keyring-backend", config.client["keyring"], "--account-number", acct, "--sequence", seq,
+                    "--chain-id", config.client["chain_id"], "--offline", "--output-document", sig_file])
             if path.isfile(sig_file):
                 signed.append({"tx": tx, "file": sig_file})
         except CalledProcessError:
